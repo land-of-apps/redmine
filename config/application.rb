cd6a7dbea7398b169b46424d7697c906cb3e6b02
Jared Kreppein <jaredkrep@gmail.com>
2020-10-01 12:05:50 -0600

    update appmap gem and add railtie

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ require 'action_view/railtie'
 require 'action_cable/engine'
 # require 'sprockets/railtie'
 require 'rails/test_unit/railtie'
+require 'appmap/railtie'
 
 Bundler.require(*Rails.groups)
 
